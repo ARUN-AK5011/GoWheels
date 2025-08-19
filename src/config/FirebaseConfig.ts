67f7f58f27c7f4295825bc8febb27615a42ba7e6
Arun AK <thedeveloper.arun@gmail.com>
2025-08-19 12:05:13 +0530

    Update FirebaseConfig.ts

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // ✅ Correct Firebase config
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAyEdktYpQV8znbC4CIVB0hVrqH3m3PGKY",
-  authDomain: "go-wheels-b0255.firebaseapp.com",
-  projectId: "go-wheels-b0255",
-  storageBucket: "go-wheels-b0255.appspot.com", // ✅ fixed .app → .appspot.com
-  messagingSenderId: "1039876666128",
-  appId: "1:1039876666128:web:f44e0c64f9583dfa23ab95", // ✅ corrected format
+  apiKey: "API KEY",
+  authDomain: "DOMAIN.com",
+  projectId: "PROJECTID",
+  storageBucket: "BUCKET", // ✅ fixed .app → .appspot.com
+  messagingSenderId: "SENDERID",
+  appId: "APPID", // ✅ corrected format
 };
 
 // Initialize Firebase App
